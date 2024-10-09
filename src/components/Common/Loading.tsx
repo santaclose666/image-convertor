5e024d507ef86eeddb2e05eb80a18ffea60cd592
Lee Cuong <cuongthinhgl123@gmail.com>
2024-10-09 22:50:15 +0700

    fix loading

--- src/components/Common/Loading.tsx
+++ src/components/Common/Loading.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 interface LoadingProps {
-  size?: number;
   className?: string;
 }
 
-function Loading({ size = 6, className }: LoadingProps) {
+function Loading({ className }: LoadingProps) {
   return (
     <div
-      className={`animate-spin rounded-full h-${size} w-${size} border-l-2 border-t-2 border-l-white border-t-white border-b-2 border-r-2 border-b-white border-r-sky-400 ${className}`}
+      className={`animate-spin rounded-full h-6 w-6 border-l-2 border-t-2 border-l-white border-t-white border-b-2 border-r-2 border-b-white border-r-sky-400 ${className}`}
     ></div>
   );
 }
